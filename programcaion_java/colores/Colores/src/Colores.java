59d9fc7d1a9042fcdb19bcac3709bf29f87f43cf
Hugo <dghugo05@gmail.com>
2024-09-30 20:59:26 +0200

    programa para ver el formateado en java

--- programcaion_java/colores/Colores/src/Colores.java
+++ programcaion_java/colores/Colores/src/Colores.java
@@ -1,12 +1,28 @@
 public class Colores {
-    public static void main(String[] args) {
-        System.out.print("\033[33m mandarina");
-        System.out.print("\033[32m hierba");
-        System.out.print("\033[31m tomate");
-        System.out.print("\033[37m sábanas");
-        System.out.print("\033[36m cielo");
-        System.out.print("\033[35m nazareno");
-        System.out.print("\033[34m mar");
-        System.out.print("\033[30m gris");
-    }
+  public static void main(String[] args) {
+    System.out.print("\033[33m mandarina");
+    System.out.print("\033[32m hierba");
+    System.out.print("\033[31m tomate");
+    System.out.print("\033[37m sábanas");
+    System.out.print("\033[36m cielo");
+    System.out.print("\033[35m nazareno");
+    System.out.print("\033[34m mar");
+    System.out.print("\033[39;49m");
+    System.out.print("\033[40m subrayado negro");
+    System.out.print("\033[39;49m");
+    System.out.print("\033[41m subrayado rojo");
+    System.out.print("\033[39;49m");
+    System.out.print("\033[42m subrayado verde");
+    System.out.print("\033[39;49m");
+    System.out.print("\033[43m subrayado amarillo");
+    System.out.print("\033[39;49m");
+    System.out.print("\033[44m subrayado azul");
+    System.out.print("\033[39;49m");
+    System.out.print("\033[45m subrayado morado");
+    System.out.print("\033[39;49m");
+    System.out.print("\033[46m subrayado cielo");
+    System.out.print("\033[39;49m");
+    System.out.print("\033[47m subrayado blanco");
+    System.out.print("\033[39;49m");
+  }
 }
