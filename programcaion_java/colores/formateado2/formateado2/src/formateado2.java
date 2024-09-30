b53809b6009b44e2d4f75b1ca5d2c160e774f98d
Hugo <dghugo05@gmail.com>
2024-09-30 21:13:52 +0200

    formateo2, numeros y margenes

---
+++ programcaion_java/colores/formateado2/formateado2/src/formateado2.java
@@ -0,0 +1,10 @@
+public class formateado2 {
+  public static void main(String[] args) {
+    System.out.println("  Artículo    Precio/caja   Nºcajas");
+    System.out.println("-------------------------------------");
+    System.out.printf("%-10s         %8.2f     %6d\n", "manzanas", 4.5, 10);
+    System.out.printf("%-10s         %8.2f     %6d\n", "peras", 2.75, 120);
+    System.out.printf("%-10s         %8.2f     %6d\n", "aguacates", 10.0, 6);
+    System.out.printf("%-10s         %8.2f     %6d\n",  "platanos", 3.17, 22);
+  }
+}
