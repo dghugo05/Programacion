a4eaefb29d62d37612affb4a71bc439a264ebd10
Hugo <dghugo05@gmail.com>
2024-10-02 19:32:01 +0200

    "Añadido apartados"

--- programcaion_java/Tema2/Ejercicio 1/Variables/src/ejercicio1.java
+++ programcaion_java/Tema2/Ejercicio 1/Variables/src/ejercicio1.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class ejercicio1 {
     public static void main(String[] args) throws Exception {
+
+/*Ejercicio 1
         float x = 144;
         float y = 999;
 
@@ -16,5 +20,33 @@ public class ejercicio1 {
         System.out.println("La resta es " + resta);
         System.out.println("La multiplicación es " + multiplicacion);
         System.out.println("La división es " + division);
+*/
+//Ejercicio 2
+/*         String nombre = "Hugo de Cristóbal Gómez";
+        System.out.println(nombre); */
+
+//Ejercicio 3
+        /* String nombre = "Hugo de Cristóbal Gómez";
+        String direccion = "Avd Andasol 14" ;
+        String telefono = "690 231 676";
+        System.out.println("Sr. " + nombre + " con dirección: " + direccion + " y número de teléfono: " + telefono); */
+
+//Ejercicio 4
+        /* Scanner sc = new Scanner(System.in);
+        double euros, pesetas;
+        System.out.println("Introduzca la cantidad de euros");
+        euros = sc.nextDouble();
+        pesetas = euros * 166.386;
+        System.out.println("La conversión de " + euros + " euros " + " a pesetas es de " + pesetas + " pesetas"); */
+
+//Ejercicio5
+        /* Scanner sc = new Scanner(System.in);
+        double pesetas, euros;
+        System.out.println("Introduzca la cantidad de pesetas que desea convertir:");
+        pesetas = sc.nextDouble();
+        euros = pesetas / 166.386;
+        System.out.println("La conversión de " + pesetas + " pesetas a euros es de : " + euros + " euros" ); */
+//Ejercicio6
+        
     }
 }
