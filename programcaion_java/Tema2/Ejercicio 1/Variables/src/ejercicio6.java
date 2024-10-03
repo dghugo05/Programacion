1545cc7350b506c3e03c394025f26c0480b07885
Hugo <dghugo05@gmail.com>
2024-10-03 20:17:53 +0200

    Ejercicios

--- programcaion_java/Tema2/Ejercicio 1/Variables/src/ejercicio6.java
+++ programcaion_java/Tema2/Ejercicio 1/Variables/src/ejercicio6.java
@@ -1,26 +1,8 @@
 import java.util.Scanner;
 
-public class ejercicio1 {
+public class ejercicio6 {
     public static void main(String[] args) throws Exception {
 
-/*Ejercicio 1
-        float x = 144;
-        float y = 999;
-
-        float suma, resta, multiplicacion, division;
-
-        suma = x + y;
-        resta = x - y;
-        multiplicacion = x * y;
-        division = x / y;
-        
-        System.out.println("El primer número es " + x);
-        System.out.println("El segundo número es " + y);
-        System.out.println("La suma es " + suma);
-        System.out.println("La resta es " + resta);
-        System.out.println("La multiplicación es " + multiplicacion);
-        System.out.println("La división es " + division);
-*/
 //Ejercicio 2
 /*         String nombre = "Hugo de Cristóbal Gómez";
         System.out.println(nombre); */
---
+++ programcaion_java/Tema2/Ejercicio2/ejercicio2/src/ejercicio2.java
@@ -0,0 +1,22 @@
+public class ejercicio2 {
+    public static void main(String[] args) throws Exception {
+
+        //Ejercicio 1
+                float x = 144;
+                float y = 999;
+        
+                float suma, resta, multiplicacion, division;
+        
+                suma = x + y;
+                resta = x - y;
+                multiplicacion = x * y;
+                division = x / y;
+                
+                System.out.println("El primer número es " + x);
+                System.out.println("El segundo número es " + y);
+                System.out.println("La suma es " + suma);
+                System.out.println("La resta es " + resta);
+                System.out.println("La multiplicación es " + multiplicacion);
+                System.out.println("La división es " + division);
+    }
+}
