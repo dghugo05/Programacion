d24e24787a2dd7061b7622644b16e6005b6ddead
Hugo2 <cghugo05@gmail.com>
2025-02-04 19:13:35 +0100

    copia codigoPostal

---
+++ programcaion_java/Tema 10/CodigoPostal copy/src/CodigoPostalDiccionario.java
@@ -0,0 +1,6 @@
+public class CodigoPostalDiccionario {
+    public static void main(String[] args) throws Exception {
+        InteraccionUsuario usuario = new InteraccionUsuario();
+        usuario.menu();
+    }
+}
